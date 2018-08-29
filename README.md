fe54122c78e200485eddec20efb2a382be27edb0
Julie-Nakadai <julienakadai@icloud.com>
2018-08-29 18:45:51 -0400

    edited the app.js and readme

--- README.md
+++ README.md
@@ -4,3 +4,22 @@ frontend-nanodegree-arcade-game
 Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
 
 For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+
+
+Frogger Arcade Game - Classic Arcade Game
+
+This is my clone of the Classic game, Frogger.
+
+To open the Game:
+clone the directory and open the index.html in browser.
+
+Rules of the Game:
+Start with 3 lives (Hearts), every successful crossing to the other side of the road will give you 3 diamonds.
+BUT... Each collision will cost you 1 heart life and 1 diamond.
+Lose 3 hearts/lives and game over!
+
+To Play the Game:
+Use the keyboard arrow keys on your keyboard to move your player up, down, right, and left around the board.
+
+How to Win:
+Successfully move your player across the board to the water without colliding with the enemy bugs.
--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
-//***********
-//Enemy class
-//***********
 
+//Enemy class
 class Enemy {
     constructor(x, y, speed) {
         this.x = x;
@@ -14,8 +12,9 @@ class Enemy {
 // Used to update enemy's position
 // parameter dt is a time delta between ticks
 Enemy.prototype.update = function (dt) {
-    // to make the game runs at same speed for all computers we multiply enemy x position by dt
+    //multiply enemy x position by dt -  to ensure game runs at same speed for all computers
     this.x += this.speed * dt;
+
     if (this.x > 520) {
         this.x = -100;
         this.speed = Math.random() * 500 + 80;
@@ -23,8 +22,8 @@ Enemy.prototype.update = function (dt) {
     this.collisionCheck();
 };
 
-//detect the collision between the user and bug
-//Source : https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+//detect the collision!
+//Source : http://blog.sklambert.com/html5-canvas-game-2d-collision-detection
 Enemy.prototype.collisionCheck = function () {
     if (player.x < this.x + 60 &&
         player.x + 50 > this.x &&
@@ -36,32 +35,27 @@ Enemy.prototype.collisionCheck = function () {
     }
 };
 
-// Draw the Enemy >:]
+// Draw the Enemy
 Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 
-//Create the Player class :)
+//the Player, life, and point - player object will hold life and point value
 class Player {
-    constructor() {
+    constructor() {  
         this.x = 200;
         this.y = 400;
         this.position = 50;
-        this.sprite = 'images/char-boy.png';
+        this.sprite = 'images/char-princess-girl.png';
         this.life = 3;
         this.point = 0;
     }
 }
 
 
-// This is to reset Player's position
-Player.prototype.resetPosition = function () {
-    this.x = 200;
-    this.y = 400;
-};
 
-//This is used so the Player remains in game boundaries
+//Player cannot move off game screen
 Player.prototype.update = function () {
 
     if (this.y > 380) {
@@ -83,24 +77,32 @@ Player.prototype.update = function () {
     }
 };
 
-//This is when the Player acheives a point
+//Point increase!
 Player.prototype.increasePoint = function () {
     this.point += 3
 };
 
-// This is when the Player loses points
+//Loses point
 Player.prototype.decreasePoint = function () {
     this.point -= 1;
     if (this.point < 0)
         this.point = 0
 };
-// This is when the Player loses a life
+// Loses a life
 Player.prototype.decreaseLife = function () {
     this.life -= 1;
     if (this.life === 0)
         player.gameOver()
 };
-// Executed when life is === 0
+
+
+// Reset player position
+Player.prototype.resetPosition = function () {
+    this.x = 200;
+    this.y = 400;
+};
+
+// Life = 0
 Player.prototype.gameOver = function () {
     if (localStorage.getItem('point') == null)
         localStorage.setItem("point", this.point);
@@ -130,40 +132,30 @@ Player.prototype.render = function () {
 };
 
 // This is the input handler
-Player.prototype.handleInput = function (keyPress) {
-    switch (keyPress) {
-        case 'left':
-            if (this.x > 0) {
-                this.x -= this.position + 50;
-            }
-            break;
-        case 'right':
-            if (this.x < 400) {
-                this.x += this.position + 50;
-            }
-            break;
-        case 'up':
-            if (this.y > 45) {
-                this.y -= this.position + 30;
-            }
-            break;
-        case 'down':
-            if (this.y < 400) {
-                this.y += this.position + 30;
-            }
-            break;
+Player.prototype.handleInput = function(direction) {
+    if(direction == 'left' && this.x > 0) {
+        this.x -= 50;
+    }
+    if(direction == 'right' && this.x < 400) {
+        this.x += 50;
+    }
+    if(direction == 'up' && this.y > 3) {
+        this.y -= 50;
+    }
+    if(direction == 'down' && this.y < 400) {
+        this.y += 50;
     }
 };
 
 
-//**********
+
 // instantiation of player and enemies
-//**********
+
 
 let allEnemies = [];
 let player = new Player();
 
-// "Y-axis" position where the enemies will are created
+// Y-axis is the position where the enemies are coming from
 let enemyY = [60, 140, 220];
 let enemy;
 enemyY.forEach(function (Y) {
@@ -171,7 +163,7 @@ enemyY.forEach(function (Y) {
     allEnemies.push(enemy);
 });
 
-// This listens for key presses and sends the keys to your
+// keypress Listener - listens to what keys you type
 document.addEventListener('keyup', function (e) {
     let allowedKeys = {
         37: 'left',
